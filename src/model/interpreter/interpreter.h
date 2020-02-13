00aa4befbb933882afcb953262791a2c589f49a7
daothauvin <daothauvin@free.fr>
2020-02-13 21:37:41 +0100

    include glib

--- src/model/interpreter/interpreter.h
+++ src/model/interpreter/interpreter.h
@@ -1,6 +1,11 @@
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
-
+//for the tree
+#include <gmodule.h>
+//for scanner
+#include <glib.h>
+#include <glib/gprintf.h>
+//arbre de syntaxe
 #include "syntax_tree.h"
 
 
