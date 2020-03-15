5644fdadbc0e1d7102a37cf5f2c88b82343e3f78
daothauvin <daothauvin@free.fr>
2020-03-15 14:20:13 +0100

    print syntax tree and find line

--- src/model/file_reader/interpreter.h
+++ src/model/file_reader/interpreter.h
@@ -1,1 +1,15 @@
-short interprete(int line, void* tree);
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+//for the tree
+#include <gmodule.h>
+#include <string.h>
+#include <stdio.h>
+#include "syntax_tree.h"
+
+int interprete(int line, void* tree, void* arena);
+
+//for test
+void print(void* tree);
+
+
+#endif
