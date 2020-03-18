f66de857274f380b54070002d1d155dbb4a128fd
LC <cliecealexis@gmail.com>
2020-03-18 17:16:49 +0100

    Implemetation of an arene

---
+++ src/model/game/arene.c
@@ -0,0 +1,42 @@
+#include "arene.h"
+#include "../../define.h"
+
+Arene create_arene(){
+    Arene a = malloc(sizeof(Arene));
+    a->list_missile = malloc(sizeof(Missile)*missile_by_robot*number_of_robots);
+    a->list_robots = malloc(sizeof(Robot)*number_of_robots);
+    a->nb_missiles = 0;
+    return a;
+}
+//Robot 3 
+// -> List_missile[4] et [5]
+int add_missile(Arene arene,Missile missile){
+    arene->list_missile[arene->nb_missiles] = missile;
+    arene->nb_missiles +=1;
+    return arene->nb_missiles - 1;
+}
+int remove_missile(Arene arene,Missile missile){
+    for(int i = 0; i  < arene->nb_missiles;++i){
+        if(arene->list_missile[i]==missile){
+            arene->list_missile[i] = arene->list_missile[arene->nb_missiles];
+            arene->nb_missiles -= 1;
+            return 0;
+        }
+    }
+    return -1;
+}
+int add_robot(Arene arene,Robot rob){
+    arene->list_robots[arene->nb_robots] = rob;
+    arene->nb_robots+=1;
+    return arene->nb_robots - 1;
+}
+int remove_robot(Arene arene,Robot rob){
+    for(int i = 0; i  < arene->nb_robots;++i){
+        if(arene->list_robots[i]==rob){
+            arene->list_robots[i] = arene->list_robots[arene->nb_robots];
+            arene->nb_robots -= 1;
+            return 0;
+        }
+    }
+    return -1;
+}
--- src/model/game/arene.h
+++ src/model/game/arene.h
@@ -2,16 +2,18 @@
 #define ARENE_H
 
 #include "robot.h"
-
+#include "missile.h"
 
 typedef struct{
 	Robot* list_robots;
 	Missile* list_missile;
+	int nb_missiles;
+	int nb_robots;
 }*Arene;
 
 Arene create_arene();
-int add_missiles(Arene arene,Missile missile);
-int remove_missiles(Arene arene,Missile missile);
+int add_missile(Arene arene,Missile missile);
+int remove_missile(Arene arene,Missile missile);
 int add_robot(Arene arene,Robot rob);
 int remove_robot(Arene arene,Robot rob);
 
