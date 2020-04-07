1b8746dbc4036d2a70584d9745340c8585accb81
daothauvin <daothauvin@free.fr>
2020-04-07 14:39:02 +0200

    cleaning files cmake

--- cmake/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dao/git/burp
-# Build directory: /home/dao/git/burp/cmake
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(check_file_reader "/home/dao/git/burp/cmake/tests/check_file_reader")
-subdirs("src")
-subdirs("tests")
