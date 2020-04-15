89650a29a48a8a473cef09bef974edd111fcf596
LC <cliecealexis@gmail.com>
2020-04-15 14:04:07 +0200

    check_function populated

---
+++ tests/check_game/check_robot.c
@@ -0,0 +1,34 @@
+#include "../../src/model/game/robot.h"
+#include <check.h>
+
+void setup(void) {}
+
+void teardown(void) {}
+
+START_TEST(create_robot)
+{
+  ;   
+}
+END_TEST
+
+Suite * check_robot (void) {
+    Suite *s;
+    TCase *tc_core, *tc_limits;
+
+    s = suite_create("Create missile");
+
+    /* Core test case */
+    tc_core = tcase_create("Core");
+
+    tcase_add_checked_fixture(tc_core, setup, teardown);
+	//tcase_add_test(tc_core, test_cardinal_and_if);
+    suite_add_tcase(s, tc_core);
+
+    /* Limits test case */
+    tc_limits = tcase_create("Limits");
+
+    //tcase_add_test(tc_limits, test_bad_expression);
+    suite_add_tcase(s, tc_limits);
+
+    return s;
+}
