dfabb6006da21933bad00dec759840f4e604a70c
daothauvin <daothauvin@free.fr>
2020-04-16 16:44:36 +0200

    interpreter error handling in main.c

--- src/main.c
+++ src/main.c
@@ -5,7 +5,21 @@
 int main(int argc, char *argv[]) {
 	
 	if(argc == 5) {
-		void* tab[4] = {init_file_tree(argv[1]),init_file_tree(argv[2]),init_file_tree(argv[3]),init_file_tree(argv[4])};
+		void* tab[4];
+		tab[0] = init_file_tree(argv[1]);
+		for(int i = 0; i < 4; i ++) {
+			int fd = open(argv[i+1],O_RDONLY);
+			if(fd <= 0 ) {
+				fprintf(stderr,"%s : Does not exist",argv[i+1]);
+			} 
+			close(fd);
+			tab[i] = init_file_tree(argv[i+1]);
+			if(tab[i] == NULL) {	
+				fprintf(stderr,"%s : %s\n",argv[i+1],message_error());
+				exit(0);
+			}
+		} 
+	
 		game(tab);
 	}
 }
