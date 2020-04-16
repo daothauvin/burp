3fdb294dc6ff49798322bb1374dc0213abb49b19
daothauvin <daothauvin@free.fr>
2020-04-16 16:45:23 +0200

    initialize robots in game.c

--- src/game.c
+++ src/game.c
@@ -7,6 +7,19 @@
 void game(void* syntax_tree[4]){	
 	Arene a = create_arene();
 	init();
+	Robot r1 = create_robot();
+	Robot r2 = create_robot();
+	Robot r3 = create_robot();
+	Robot r4 = create_robot();
+	initialize_robot(r1,0,0,0,0,0);
+	initialize_robot(r2,0,9999.0,0,0,1);
+	initialize_robot(r3,9999.0,9999.0,0,0,1);
+	initialize_robot(r4,9999.0,0,0,0,4);
+	add_robot(a,r1);
+	add_robot(a,r2);
+	add_robot(a,r3);
+	add_robot(a,r4);
+	
 	int line[4] = {0};
 
 	while (cycle(a, line, syntax_tree)){
