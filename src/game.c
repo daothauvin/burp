3b21c710e03f3232e7c91d464eb7c8fbbfab799f
Thomas Copt-Bignon <lowx.add@gmail.com>
2020-04-23 18:54:50 +0200

    fix updateArena()

--- src/game.c
+++ src/game.c
@@ -7,28 +7,30 @@
 void game(Tree syntax_tree[4])
 {
 	arena *a = create_arena();
-	init();
-	robot* r1 = create_robot(5000,5000,0,100,0);
-	robot* r2 = create_robot(0,10000.0,0,0,1);
-	robot* r3 = create_robot(10000.0,10000.0,0,0,2);
-	robot* r4 = create_robot(10000.0,0,0,0,3);
+
+	//                        X        Y      Angle  Spd   Id
+	robot* r0 = create_robot(0.0,     0.0,     0.0,  0.0,  0);
+	robot* r1 = create_robot(0.0,     9999.0,  0.0,  0.0,  1);
+	robot* r2 = create_robot(9999.0,  9999.0,  0.0,  0.0,  2);
+	robot* r3 = create_robot(9999.0,  0.0,     0.0,  0.0,  3);
+
+	add_robot(a,r0);
 	add_robot(a,r1);
 	add_robot(a,r2);
 	add_robot(a,r3);
-	add_robot(a,r4);
 	
 	int line[4] = {0};
+	init();
+	updateArena(a);
+	// anim_begin();
 
 	while (cycle(a, line, syntax_tree)){
-		
 		updateArena(a);
 		for(int i = 0;i < get_nb_robot_arena(a); i++) {
 			add_action(getNextCommand(i),i);
 		}
-		
 		if(!waitForInput())
 			break;
-		//printf("one turn\n");
 	}
 	quit();
 }
