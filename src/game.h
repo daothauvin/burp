caf6ca971550ff55c70b66d931585b4671ae2f70
daothauvin <daothauvin@free.fr>
2020-05-13 19:34:03 +0200

    change doxygen

--- src/game.h
+++ src/game.h
@@ -2,10 +2,6 @@
 #include "controller/controller.h"
 #include "model/game/arene.h"
 #include "model/game/robot.h"
-<<<<<<< HEAD
-=======
-#include "model/file_reader/syntax_tree.h"
->>>>>>> 84c86f354de469ff8726b58026ed8e8ff0c1a296
 #include "model/cycle.h"
 /// \cond
 #include <stdio.h>
